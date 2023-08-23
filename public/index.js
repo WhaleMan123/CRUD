419833ea4f5ddcc6464a91f0dd2f47c3da72bedb
boram-blockchain <penguniee@gmail.com>
2023-08-23 17:34:21 +0900

    featt: 변수 수정 완료

--- public/index.js
+++ public/index.js
@@ -19,19 +19,19 @@
 // ]
 
 // localStorage.setItem("board",JSON.stringify(boardValue))
-const board = localStorage.getItem("board")
+const list_board = localStorage.getItem("board")
   ? JSON.parse(localStorage.getItem("board"))
   : [];
 const list_tbody = document.querySelector("tbody");
 
-for (let i = 0; i < board.length; i++) {
+for (let i = 0; i < list_board.length; i++) {
   const list_tableRow = () => `
     <tr>
-        <td>${board[i].uid}</td>
-        <td><a href="../view/index.html?uid=${board[i].uid}">${board[i].title}</a></td>
-        <td>${board[i].writer}</td>
-        <td>${board[i].createdAt}</td>
-        <td>${board[i].hit}</td>
+        <td>${list_board[i].uid}</td>
+        <td><a href="../view/index.html?uid=${list_board[i].uid}">${list_board[i].title}</a></td>
+        <td>${list_board[i].writer}</td>
+        <td>${list_board[i].createdAt}</td>
+        <td>${list_board[i].hit}</td>
     </tr>
     `;
   list_tbody.innerHTML += list_tableRow();
