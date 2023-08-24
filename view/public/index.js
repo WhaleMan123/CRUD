54c56bf61c0a5da643638198f1661754e98791e4
chapman-blockchain <limth1312@naver.com>
2023-08-24 09:33:01 +0900

    feat:view 작성 중 / 글 불러오기 구현 완료

--- view/public/index.js
+++ view/public/index.js
@@ -9,18 +9,18 @@
 //   },
 // ];
 
-// const value = JSON.stringify([
-//   {
-//     uid: 1,
-//     title: "제목입니다.",
-//     writer: "작성자",
-//     createdAt: "2023-08-23 15:03:45",
-//     content: "글의 내용입니다.",
-//     hit: 421,
-//   },
-// ]);
+const value = JSON.stringify([
+  {
+    uid: 1,
+    title: "제목입니다.",
+    writer: "작성자",
+    createdAt: "2023-08-23 15:03:45",
+    content: "글의 내용입니다.",
+    hit: 421,
+  },
+]);
 
-// localStorage.setItem("board", value);
+localStorage.setItem("board", value);
 // location.href = `/view/index.html?id=${view_i}`;
 
 // --------------------------------------------------------------
@@ -29,19 +29,19 @@ const view_i = Number(location.href.split("=")[1]);
 
 const view_title = document.getElementById("view-title");
 const view_titleInput = JSON.parse(localStorage.getItem("board"))[view_i].title;
-title.innerText = titleInput;
+view_title.innerText = view_titleInput;
 
 const view_writer = document.getElementById("view-writer");
 const view_writerInput = JSON.parse(localStorage.getItem("board"))[view_i]
   .writer;
-writer.innerText = writerInput;
+view_writer.innerText = view_writerInput;
 
 const view_createdAt = document.getElementById("view-createdAt");
 const view_createdAtInput = JSON.parse(localStorage.getItem("board"))[view_i]
   .createdAt;
-createdAt.innerText = createdAtInput;
+view_createdAt.innerText = view_createdAtInput;
 
 const view_content = document.getElementById("view-content");
 const view_contentInput = JSON.parse(localStorage.getItem("board"))[view_i]
   .content;
-content.innerText = contentInput;
+view_content.innerText = view_contentInput;
