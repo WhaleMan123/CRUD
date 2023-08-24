e476bfb153e2e3d5ba0634b39f92442777a5be67
chapman-blockchain <limth1312@naver.com>
2023-08-24 11:06:09 +0900

    feat:view 작성 중 / 출력 및 삭제버튼 uid 활용기능 구현 완료

--- view/public/index.js
+++ view/public/index.js
@@ -27,36 +27,44 @@
 // --------------------------------------------------------------
 
 const view_i = Number(location.href.split("=")[1]);
+const view_objArr = JSON.parse(localStorage.getItem("board"));
 
-const view_title = document.getElementById("view-title");
-const view_titleInput = JSON.parse(localStorage.getItem("board"))[view_i].title;
-view_title.innerText = view_titleInput;
+for (let i = 0; i < view_objArr.length; i++) {
+  if (view_objArr[i].uid === view_i) {
+    const view_title = document.getElementById("view-title");
+    const view_titleInput = view_objArr[i].title;
+    view_title.innerText = view_titleInput;
 
-const view_writer = document.getElementById("view-writer");
-const view_writerInput = JSON.parse(localStorage.getItem("board"))[view_i]
-  .writer;
-view_writer.innerText = view_writerInput;
+    const view_writer = document.getElementById("view-writer");
+    const view_writerInput = view_objArr[i].writer;
+    view_writer.innerText = view_writerInput;
 
-const view_createdAt = document.getElementById("view-createdAt");
-const view_createdAtInput = JSON.parse(localStorage.getItem("board"))[view_i]
-  .createdAt;
-view_createdAt.innerText = view_createdAtInput;
+    const view_createdAt = document.getElementById("view-createdAt");
+    const view_createdAtInput = view_objArr[i].createdAt;
+    view_createdAt.innerText = view_createdAtInput;
 
-const view_content = document.getElementById("view-content");
-const view_contentInput = JSON.parse(localStorage.getItem("board"))[view_i]
-  .content;
-view_content.innerText = view_contentInput;
+    const view_content = document.getElementById("view-content");
+    const view_contentInput = view_objArr[i].content;
+    view_content.innerText = view_contentInput;
+
+    break;
+  }
+}
 
 // 삭제 버튼 구현
 const view_deleteBtn = document.querySelector(".delete-btn");
 
 function handelDeleteBtn() {
-  const view_boardArray = JSON.parse(localStorage.getItem("board"));
-  view_boardArray.splice(view_i, 1);
-
-  const view_boardArrayStr = JSON.stringify(view_boardArray);
-  localStorage.setItem("board", view_boardArrayStr);
-  location.href = "../index.html";
+  for (let i = 0; i < view_objArr.length; i++) {
+    if (view_objArr[i].uid === view_i) {
+      const view_boardArray = JSON.parse(localStorage.getItem("board"));
+      view_boardArray.splice(i, 1);
+      const view_boardArrayStr = JSON.stringify(view_boardArray);
+      localStorage.setItem("board", view_boardArrayStr);
+      location.href = "../index.html";
+      break;
+    }
+  }
 }
 
 view_deleteBtn.addEventListener("click", handelDeleteBtn);
